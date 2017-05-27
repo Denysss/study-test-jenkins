04cf8e63e3e76a78b4d088bdf16bac4aad726465
Denys Shynkarenko <denys_shynkarenko1@epam.com>
2017-05-27 17:33:50 +0300

    first

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,22 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class MyTests {
+
+    @Test
+    public void firstTest() {
+        Assert.assertEquals(true, true);
+    }
+
+    @Test
+    public void secondTest() {
+        Assert.assertTrue(new Random().nextBoolean(), "secondTest - 50 %");
+    }
+
+/*    @Test
+    public void thirdTest() {
+        throw new RuntimeException(this.toString());
+    }*/
+}
